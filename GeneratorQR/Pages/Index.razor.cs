90ad6769da4a5db43d5722c9047e061a43be89c7
Jhonny P <jhonny.perea@outlook.com>
2022-12-08 15:36:27 -0500

    Agregar archivos de proyecto.

---
+++ GeneratorQR/Pages/Index.razor.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+using QRCoder;
+
+namespace GeneratorQR.Pages
+{
+    public partial class Index
+    {
+        MudForm urlSubtimForm;
+        public string submittedUrl { get; set; }
+        public string QRCodeText { get; set; }
+
+        private async Task submitUrl()
+        {
+            await urlSubtimForm.Validate();
+            if(urlSubtimForm.IsValid)
+                GenerateQRCode();
+        }
+
+        protected void GenerateQRCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(submittedUrl, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qRCodeData);
+            byte[] qrCodeAsByteArr = qrCode.GetGraphic(20);
+
+            var ms = new MemoryStream(qrCodeAsByteArr);
+
+            QRCodeText = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+            submittedUrl = String.Empty;
+
+
+
+
+        }
+    }
+}
